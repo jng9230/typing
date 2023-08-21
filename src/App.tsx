1f23ab53ce2bbc711a0f5bed5b37bfc24c12b1db
John <jng9230@gmail.com>
2023-08-20 22:13:43 -0400

    wpm/acc init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { type } from "os";
 import { generate, count } from "random-words";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { BiRevision } from "react-icons/bi"
 
 function App() {
@@ -17,14 +17,13 @@ function App() {
     })
   })
   const [typedWords, setTypedWords] = useState<Boolean[]>([])
-
+  const [finished, setFinished] = useState(false);
   const [wordIndex, setWordIndex] = useState(0)
   const [inputText, setInputText] = useState("")
   const handleInputText = (e: React.FormEvent<HTMLInputElement>) => {
     const str = e.currentTarget.value
     const currChar = str[str.length - 1]
     const currWord = str.slice(0, str.length - 1)
-
 
     //update indexes and check word if it's a space
     if (currChar === " ") {
@@ -36,7 +35,25 @@ function App() {
 
     //continue otherwise
     setInputText(e.currentTarget.value)
+
+    //last word -> end time, calculate accuracy
+    if (wordIndex === numWords - 1 &&
+      str.length === wordsArr[numWords - 1].length
+    ) {
+      setFinished(true)
+      setInputText("")
+      setTypedWords([...typedWords, currWord === wordsArr[wordIndex]])
+      return;
+    }
   }
+
+  useEffect(() => {
+    if (finished) {
+      setWPM(111)
+      setACC(111)
+      setFinished(false)
+    }
+  }, [finished])
 
   //reset test if tab is pressed 
   const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -44,25 +61,18 @@ function App() {
 
     e.preventDefault();
 
-    resetWords();
+    resetWords(numWords);
   }
-  const WPM = 0;
-  const ACC = 0;
+  const [WPM, setWPM] = useState<"NAN" | number>("NAN");
+  const [ACC, setACC] = useState<"NAN" | number>("NAN")
 
   const updateNumWords = (num: number) => {
     setNumWords(num)
-    const newGen = generate({ exactly: num });
-    setWordsArr(newGen)
-    setWords(newGen.reduce((acc, curr) => {
-      return acc + " " + curr
-    }))
-    setWordIndex(0);
-    setTypedWords([]);
-    setInputText("");
-
+    resetWords(num);
   }
-  const resetWords = () => {
-    const newGen = generate({ exactly: numWords })
+
+  const resetWords = (num: number) => {
+    const newGen = generate({ exactly: num })
     setWordsArr(newGen)
     setWords(newGen.reduce((acc, curr) => {
       return acc + " " + curr
@@ -71,7 +81,10 @@ function App() {
     setInputText("")
     setWordIndex(0)
     setTypedWords([])
+    setACC("NAN")
+    setWPM("NAN")
   }
+
   return (
     <div className="w-screen h-screen flex items-center justify-center font-mono">
       <div className="p-3 space-y-3 w-2/3">
@@ -94,13 +107,13 @@ function App() {
               WPM: {WPM}
             </span>
             <span className="px-3 pr-0 py-1">
-              ACC: {ACC}
+              ACC: {ACC}%
             </span>
           </div>
         </div>
         <div className=" text-gray-500 relative">
           {words}
-          <div className=" absolute top-0 z-50">
+          <div className="absolute top-0 left-0 z-50">
             {
               typedWords?.map((correct, i) => {
                 const textColor = correct ? "text-green-500" : "text-red-500"
@@ -123,7 +136,7 @@ function App() {
             autoFocus
           >
           </input>
-          <button onClick={() => { resetWords() }} className="p-2 hover:border-red-500 hover:text-red-500 border-2 border-black rounded-lg"
+          <button onClick={() => { resetWords(numWords) }} className="p-2 hover:border-red-500 hover:text-red-500 border-2 border-black rounded-lg"
             title="Reset"
           >
             <BiRevision />
